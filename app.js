d1669b3eff3a0864ec00304b08adf937fad1cc32
mathuramg <mmg9291@gmail.com>
2016-04-02 13:36:08 -0400

    initial commit

--- app.js
+++ app.js
@@ -42,74 +42,71 @@ app.get('/login', function (req,res){
 		console.log("YOU ARE AUTHENTICATED");
 		var apiClient = c.apiCall('/api/client/', function (apiClient){
 			// console.log(apiClient);
-			// console.log(' ----- ');
+			console.log(' ----- ');
 			var clientInfo = JSON.parse(apiClient);
 			console.log(clientInfo);
 			clientData.uuid = clientInfo[0].id;
 			clientData.locationID = clientInfo[0].locations[0];
-			res.send(clientData);
+			res.redirect('/atITP');
 		});
 	});
 
 });
 
 
-// Returns client information, saves the important bits for later
-// app.get('/client', function (req,res){
-//
-// });
+/*******************************************
+Code to get when the coffee machine is used
+*******************************************/
+var equipId;
+app.get('/atITP', function (req,res){
+
+	var locationId = clientData.locationID;
+
+	/****************************************
+	GET CURRENT TIME
+	****************************************/
+	var endTime = new Date();
+	var endTimeFormatted = endTime.toISOString();
+
+	/****************************************
+	GET TIME N HOURS BEFORE
+	****************************************/
+	var noOfHours = 24*6;
+	var interval = '15min';
+
+	var startTime = new Date( endTime.getTime() - noOfHours*60*60*1000);
+	var startTimeFormatted = startTime.toISOString();
 
 
+	//var equipId = JSON.parse(atITP)[0].id;
+  //Dish washer
+	var equipId = '5a2ed6fe-05a9-42d9-8797-88045412c05a';
 
-// Example showing top 10 energy consumers
-// Uses the client uuid we saved from '/client'
-app.get('/getTop', function (req,res){
-	// Set the date range you want to examine
-	var start = encodeURIComponent('2015-01-01 00:00:00Z');
-	var end = encodeURIComponent('2015-01-03 00:00:00Z');
-	var client = clientData.uuid;
+	//Coffee machine
+	//var equipId = '28b35713-2259-453a-882e-65408aec2bca';
 
-	var top10 = c.apiCall('/api/client/'+client+'/top_consumers/?count=10&fromTime='+start+'&toTime='+end, function (top10){
-		console.log(JSON.parse(top10));
-		topData.push(JSON.parse(top10));
-		res.end();
-		for(var i =0;i<JSON.parse(top10).now.total.length;i++)
+	startTimeFormatted = encodeURIComponent('2015-01-01 00:00:00Z');
+	endTimeFormatted = encodeURIComponent('2015-09-01 00:00:00Z');
+
+
+	var detailOfEquipmentUrl = '/api/equipment/' + equipId + '/data/?fromTime=' + startTimeFormatted +'&toTime='+ endTimeFormatted + '&interval=day&cost=true';
+	var laser = c.apiCall(detailOfEquipmentUrl, function(laser){
+		var b = JSON.parse(laser);
+		var c = b.data;
+		//sent just the required data
+		console.log(c);
+		var data = [];
+		for(var i=0;i<c.length;i++)
 		{
-			console.log(JSON.parse(top10).now.total[0]);
+			var xAxis = i*15;
+			//var yAxis = c[i]["Coffee MakerTotalCost"]
+			var yAxis = c[i]["DishwasherTotalCost"];
+			console.log(yAxis);
+			data[i] = {x:xAxis, y:yAxis};
 		}
+
+		res.send(data);
+
 	});
-});
 
-
-// Get energy and cost data by location
-// Uses the location uuid we saved from '/client'
-app.get('/getEnergy', function (req,res){
-	var location = clientData.locationID;
-	var startdate = encodeURIComponent('2015-01-01 00:00:00Z');
-	var enddate = encodeURIComponent('2015-09-01 00:00:00Z')
-	var interval = 'month';
-
-	var energy = c.apiCall('/api/location/'+location+'/data/?fromTime='+startdate+'&toTime='+enddate+'&interval='+interval+'&cost=true', function (energy){
-	    console.log(JSON.parse(energy));
-	    energyData.push(JSON.parse(energy));
-	    res.end()
-	});
-});
-
-app.get('/getEq', function (req,res){
-var id =  '3e9815ef-af63-4ca1-af80-641885547931';
-console.log(clientData);
-var equip = c.apiCall('/api/equipment/'+id + '/data/', function (equip){
-		console.log(JSON.parse(equip));
-		equipData.push(JSON.parse(equip));
-		res.end()
-});
-});
-
-app.get('/top10', function (req,res){
-	res.send(topData);
-});
-
-app.get('/energy', function (req,res){
-	res.send(energyData);
 });
